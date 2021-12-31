afbfc05981dbfecbe8d98c9f195dd66e7476af05
Aaron Dickey <aaron@onedaynerd.com>
2021-12-31 10:16:08 -0500

    Initial Commit

---
+++ PhotoMetaDataManager/PhotoMetaDataManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMetaDataManagerApp.swift
+//  PhotoMetaDataManager
+//
+//  Created by Aaron Dickey on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMetaDataManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
